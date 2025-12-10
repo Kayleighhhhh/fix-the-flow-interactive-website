30f3f5a70e4815332c492d5582d1845bcdb7b0e7
Kayleighhhhh <kayleigh.vdham04@gmail.com>
2025-12-10 11:32:21 +0100

    Add multi-select filtering for book list #25
    Replaces single select filter with support for multiple select elements (author, year, place) to filter the book list. Updates event listeners and filtering logic to show or hide list items based on selected filter values.

--- scripts/script.js
+++ scripts/script.js
@@ -8,20 +8,76 @@ menuBtn.addEventListener("click", toggleMenu);
 
 // wat het event moet doen als er op de button geklikt wordt
 function toggleMenu() {
-    deNav.classList.toggle("is-open");
-    menuBtn.classList.toggle("is-open");
+  deNav.classList.toggle("is-open");
+  menuBtn.classList.toggle("is-open");
 }
 
+// // de select met de filter categorien zoeken
+// let deSelect = document.querySelector('select');
+
+// // de ul met alle articles zoeken
+// let deLijst = document.querySelector('ul');
+
+// // event voor het veranderen van de filter toevoegen
+// deSelect.addEventListener("change", filter);
+
+// function filter(event) {
+// // het event de taget value class toe laten voegen aan de ul
+//   deLijst.dataset.filter = event.target.value;
+// }
+
 // de select met de filter categorien zoeken
-let deSelect = document.querySelector('select');
+let allSelect = document.querySelectorAll('select');
+
+let deBoeken = document.querySelectorAll('li');
+console.log(deBoeken);
 
 // de ul met alle articles zoeken
-let deLijst = document.querySelector('ul');
+// let deLijst = document.querySelector('ul');
 
 // event voor het veranderen van de filter toevoegen
-deSelect.addEventListener("change", filter);
+allSelect.forEach(function (select) {
+  select.addEventListener("change", filter);
+})
+
 
 function filter(event) {
-// het event de taget value class toe laten voegen aan de ul
-  deLijst.dataset.filter = event.target.value;
+
+  // let Filter = event.target.value;
+
+  let auteurFilter = allSelect[0].value;
+  let jaarFilter = allSelect[1].value;
+  let plaatsFilter = allSelect[2].value;
+
+
+  //   if (hetFilter == "all") {
+  //     deBoeken.forEach(eenBoek => {
+  //         eenBoek.classList.remove("hidden");
+  //     });
+
+
+
+// else {
+deBoeken.forEach(eenBoek => {
+
+  if (
+    //         auteur of all
+
+    (eenBoek.classList.contains(auteurFilter) || auteurFilter == "all")
+    // en
+
+    &&
+    //         jaar of all
+    (eenBoek.classList.contains(jaarFilter) || jaarFilter == "all")
+    // en
+
+    &&
+    //         plaats of all
+    (eenBoek.classList.contains(plaatsFilter) || plaatsFilter == "all")
+  ) {
+    eenBoek.classList.remove("hidden");
+  } else {
+    eenBoek.classList.add("hidden");
+  }
+});
 }
